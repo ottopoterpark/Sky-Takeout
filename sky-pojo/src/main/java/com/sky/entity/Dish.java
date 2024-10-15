6ca3c2144c1698c7bca4d48a32a835e07b3a8f13
ottopoterpark <11608356+uniquealuo@user.noreply.gitee.com>
2024-10-15 18:18:43 +0800

    外卖点餐平台-菜品分页查询

--- sky-pojo/src/main/java/com/sky/entity/Dish.java
+++ sky-pojo/src/main/java/com/sky/entity/Dish.java
@@ -25,32 +25,22 @@ public class Dish implements Serializable {
 
     @TableId(type = IdType.AUTO)
     private Long id;
-
     // 菜品名称
     private String name;
-
     // 菜品分类id
     private Long categoryId;
-
     // 菜品价格
     private BigDecimal price;
-
     // 图片
     private String image;
-
     // 描述信息
     private String description;
-
     // 0 停售 1 起售
     private Integer status;
-
     // 公共字段
     private LocalDateTime createTime;
-
     private LocalDateTime updateTime;
-
     private Long createUser;
-
     private Long updateUser;
 
 }
--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,23 +1,24 @@
 package com.sky.controller.admin;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.toolkit.Db;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.exception.BaseException;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.desktop.QuitEvent;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -42,5 +43,27 @@ public class DishController {
         BeanUtils.copyProperties(dishDTO,dish);
         dishService.saveDish(dish,dishDTO);
         return Result.success();
+    }
+
+    /**
+     * 菜品分页查询
+     * @param query
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO query)
+    {
+        log.info("菜品分页查询");
+        Page<Dish> page = Page.of(query.getPage(), query.getPageSize());
+        dishService.lambdaQuery()
+            .like(query.getName() != null, Dish::getName, query.getName())
+            .eq(query.getCategoryId() != null, Dish::getCategoryId, query.getCategoryId())
+            .eq(query.getStatus() != null, Dish::getStatus, query.getStatus())
+            .page(page);
+        PageResult data = PageResult.builder()
+                .total(page.getTotal())
+                .records(page.getRecords())
+                .build();
+        return Result.success(data);
     }
 }
