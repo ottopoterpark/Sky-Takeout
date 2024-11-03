9ae0536ced87e1bf972248fe499012e97ce5e54d
ottopoterpark <11608356+uniquealuo@user.noreply.gitee.com>
2024-11-03 23:59:13 +0800

    管理端查询订单管理数据

--- sky-server/src/main/java/com/sky/controller/admin/WorkspaceController.java
+++ sky-server/src/main/java/com/sky/controller/admin/WorkspaceController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.WorkspaceService;
 import com.sky.vo.BusinessDataVO;
+import com.sky.vo.OrderOverViewVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkspaceController {
 
     @Autowired
-    private WorkspaceService workplaceService;
+    private WorkspaceService workspaceService;
 
     /**
      * 查询今日运营数据
@@ -28,7 +29,19 @@ public class WorkspaceController {
     public Result<BusinessDataVO> businessData()
     {
         log.info("查询今日运营数据");
-        BusinessDataVO businessDataVO=workplaceService.businessData();
+        BusinessDataVO businessDataVO= workspaceService.businessData();
         return Result.success(businessDataVO);
+    }
+
+    /**
+     * 查询订单管理数据
+     * @return
+     */
+    @GetMapping("/overviewOrders")
+    public Result<OrderOverViewVO> overviewOrders()
+    {
+        log.info("查询订单管理数据");
+        OrderOverViewVO orderOverViewVO=workspaceService.overviewOrders();
+        return Result.success(orderOverViewVO);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/WorkspaceServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/WorkspaceServiceImpl.java
@@ -5,6 +5,7 @@ import com.sky.entity.Orders;
 import com.sky.entity.User;
 import com.sky.service.WorkspaceService;
 import com.sky.vo.BusinessDataVO;
+import com.sky.vo.OrderOverViewVO;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -65,6 +66,34 @@ public class WorkspaceServiceImpl implements WorkspaceService {
                 .orderCompletionRate(orderCompletionRate)
                 .unitPrice(unitPrice)
                 .newUsers(newUsers)
+                .build();
+    }
+
+    /**
+     * 查询订单管理数据
+     * @return
+     */
+    @Override
+    public OrderOverViewVO overviewOrders()
+    {
+        // 获取所有订单
+        List<Orders> orders = Db.lambdaQuery(Orders.class)
+                .list();
+
+        // 分别统计各种类型的订单
+        Integer waitingOrders = (int) orders.stream().filter(o -> o.getStatus().equals(Orders.TO_BE_CONFIRMED)).count();
+        Integer deliveredOrders = (int) orders.stream().filter(o -> o.getStatus().equals(Orders.CONFIRMED)).count();
+        Integer completedOrders = (int) orders.stream().filter(o -> o.getStatus().equals(Orders.COMPLETED)).count();
+        Integer cancelledOrders = (int) orders.stream().filter(o -> o.getStatus().equals(Orders.CANCELLED)).count();
+        Integer allOrders = (int) orders.size();
+
+        // 返回结果
+        return OrderOverViewVO.builder()
+                .waitingOrders(waitingOrders)
+                .deliveredOrders(deliveredOrders)
+                .completedOrders(completedOrders)
+                .cancelledOrders(cancelledOrders)
+                .allOrders(allOrders)
                 .build();
     }
 }
