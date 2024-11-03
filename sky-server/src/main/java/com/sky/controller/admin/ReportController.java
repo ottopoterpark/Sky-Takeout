ef35cc9775b3c05cba8ff8c309207bc84c5b6125
ottopoterpark <11608356+uniquealuo@user.noreply.gitee.com>
2024-11-03 20:24:15 +0800

    管理端用户统计功能

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -39,5 +40,21 @@ public class ReportController {
         log.info("营业额统计接口: {}  {}", begin, end);
         TurnoverReportVO turnoverReportVO=reportService.turnoverStatistics(begin,end);
         return Result.success(turnoverReportVO);
+    }
+
+    /**
+     * 用户统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/userStatistics")
+    public Result<UserReportVO> userStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end)
+    {
+        log.info("用户统计接口: {}  {}", begin, end);
+        UserReportVO userReportVO= reportService.userStatistics(begin,end);
+        return Result.success(userReportVO);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,9 +1,12 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.extension.toolkit.Db;
 import com.sky.entity.Orders;
+import com.sky.entity.User;
 import com.sky.service.OrdersService;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,16 +14,12 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class ReportServiceImpl implements ReportService {
-
-    @Autowired
-    private OrdersService ordersService;
 
     /**
      * 营业额统计
@@ -33,7 +32,7 @@ public class ReportServiceImpl implements ReportService {
     public TurnoverReportVO turnoverStatistics(LocalDate begin, LocalDate end)
     {
         // 获得每一天的订单
-        Map<LocalDate, List<Orders>> orders = ordersService.lambdaQuery()
+        Map<LocalDate, List<Orders>> orders = Db.lambdaQuery(Orders.class)
                 .eq(Orders::getStatus, Orders.COMPLETED)
                 .ge(Orders::getCheckoutTime, begin.atStartOfDay())
                 .lt(Orders::getCheckoutTime, end.plusDays(1).atStartOfDay())
@@ -79,6 +78,71 @@ public class ReportServiceImpl implements ReportService {
         return TurnoverReportVO.builder()
                 .dateList(dateList)
                 .turnoverList(turnoverList)
+                .build();
+    }
+
+    /**
+     * 用户统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO userStatistics(LocalDate begin, LocalDate end)
+    {
+        // 获取每一天的注册人数
+        List<User> users = Db.lambdaQuery(User.class)
+                .lt(end != null, User::getCreateTime, end.plusDays(1).atStartOfDay())
+                .list();
+        Map<LocalDate, List<User>> userList = users.stream().collect(Collectors.groupingBy(u -> u.getCreateTime().toLocalDate()));
+
+        // 计算begin之前的总人数
+        final LocalDate tmpBegin = begin;
+        long total = users.stream().filter(u ->
+        {
+            return u.getCreateTime().isBefore(tmpBegin.atStartOfDay());
+        }).count();
+
+        // 获得范围里的每一天
+        List<LocalDate> dates=new ArrayList<>();
+        dates.add(begin);
+        while (!begin.equals(end))
+        {
+            begin = begin.plusDays(1);
+            dates.add(begin);
+        }
+
+        // 获得符合格式要求的日期字符串
+        String dateList = StringUtils.join(dates, ",");
+
+        // 每天新增人数和总人数
+        List<Long> newUsers=new ArrayList<>();
+        List<Long> totalUsers=new ArrayList<>();
+
+        // 计算结果
+        for (LocalDate date : dates)
+        {
+            List<User> tmpUsers = userList.get(date);
+            if(tmpUsers==null||tmpUsers.isEmpty())
+            {
+                newUsers.add(0L);
+                totalUsers.add(0L);
+                continue;
+            }
+            total+=userList.get(date).size();
+            newUsers.add((long) userList.get(date).size());
+            totalUsers.add(total);
+        }
+
+        // 获得符合格式要求的字符串
+        String newUserList = StringUtils.join(newUsers, ",");
+        String totalUserList = StringUtils.join(totalUsers, ",");
+
+        // 返回结果
+        return UserReportVO.builder()
+                .dateList(dateList)
+                .newUserList(newUserList)
+                .totalUserList(totalUserList)
                 .build();
     }
 }
